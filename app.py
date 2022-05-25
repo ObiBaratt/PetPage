736360caf558b3646a345be7086cbfe2908b4f00
Obi Baratt <obibaratt@gmail.com>
2022-05-24 23:52:14 -0700

    Utilized Start Bootstrap Grayscale theme for snow.html (and future other theme pages), added snow route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', msg='Hello This is Roka\'s Homepage.')
+
+
+@app.route('/snow')
+def snow():
+    return render_template('snow.html')
 
 
 if __name__ == "__main__":
